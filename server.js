a129c40fd6af79c5b59949537877e7bf37f189c3
leon123858 <a0970785699@gmail.com>
2020-06-30 01:27:05 +0800

    asd

--- server.js
+++ server.js
@@ -6,20 +6,7 @@ const nconf = require('nconf');
 
 // Read in keys and secrets. Using nconf use can set secrets via
 // environment variables, command-line arguments, or a keys.json file.
-nconf.argv().env().file('keys.json');
-
-// Connect to a MongoDB server provisioned over at
-// MongoLab.  See the README for more info.
-
-const user = nconf.get('mongoUser');
-const pass = nconf.get('mongoPass');
-const host = nconf.get('mongoHost');
-const port = nconf.get('mongoPort');
-
-let uri = `mongodb://${user}:${pass}@${host}:${port}`;
-if (nconf.get('mongoDatabase')) {
-  uri = `${uri}/${nconf.get('mongoDatabase')}`;
-}
+var uri = "mongodb+srv://leon1234858:8ntscpal@cluster0.gyixj.gcp.mongodb.net/sample_airbnb?retryWrites=true&w=majority";
 console.log(uri);
 
 mongodb.MongoClient.connect(uri, (err, db) => {
